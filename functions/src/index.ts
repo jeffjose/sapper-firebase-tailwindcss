f680d92d501afbf10b069773a97153ec78769679
Jeffrey Jose <jeffjosejeff@gmail.com>
2020-09-22 19:23:50 -0700

    formatting

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,7 +4,6 @@ import * as functions from "firebase-functions";
 // https://firebase.google.com/docs/functions/typescript
 
 export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
+  functions.logger.info("Hello logs!", { structuredData: true });
   response.send("Hello from Firebase!");
 });
-
